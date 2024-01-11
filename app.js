4cc6bfa6451f032762cb7722215cd3a320c70f83
mkartikeyy <mkartikey666@gmail.com>
2024-01-11 16:05:54 +0530

    creating by myself

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
-console.log("hitesh")
-const buttons = document.querySelectorAll('.button');
-const body = document.querySelector('body');
+const buttons = document.querySelectorAll('.box')
+const body = document.querySelector('body')
 
-buttons.forEach(function (button) {
-  console.log(button);
-  button.addEventListener('click', function (e) {
-    console.log(e);
-    console.log(e.target);
-    if (e.target.id === 'grey') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'white') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'blue') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'yellow') {
-      body.style.backgroundColor = e.target.id;
-    }
-    
-  });
-});
+
+buttons.forEach(function(button){
+    button.addEventListener('click',function(event){
+        if(event.target.id === 'blue')
+            body.style.backgroundColor = event.target.id;
+        if(event.target.id === 'purple')
+            body.style.backgroundColor = event.target.id;
+        if(event.target.id === 'green')
+            body.style.backgroundColor = event.target.id;
+        if(event.target.id === 'orange')
+            body.style.backgroundColor = event.target.id;
+    })
+})
